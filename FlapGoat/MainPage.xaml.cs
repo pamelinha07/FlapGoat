ea95b342e7129a48061cf6f56007e5e4e1cf8d73
Pamela <pamela.isabeli167@gmail.com>
2024-10-11 08:50:16 -0300

    OK

--- FlapGoat/MainPage.xaml.cs
+++ FlapGoat/MainPage.xaml.cs
@@ -2,9 +2,12 @@
 
 public partial class MainPage : ContentPage
 {
-	const int gravidade = 5;
+	const int gravidade = 3;
 	const int tempoEntreFrames = 25;
 	bool estaMorto = true;
+	double larguraJanela = 0;
+	double alturaJanela = 0;
+	int velocidade = 15;
 
 	public MainPage()
 	{
@@ -23,6 +26,7 @@ public partial class MainPage : ContentPage
 		{
 			AplicaGravidade();
 			await Task.Delay(tempoEntreFrames);
+			GerenciaCanos();
 		}
 	}
 
@@ -37,6 +41,24 @@ public partial class MainPage : ContentPage
 		estaMorto = false;
 		imgPersonagem.TranslationY = 0;
 	}
+	protected override void OnSizeAllocated(double w, double h)
+	{
+      base.OnSizeAllocated(w, h);
+	  larguraJanela = w;
+	  alturaJanela = h;
+	}
+	void GerenciaCanos()
+	{
+		imgTroncoCima.TranslationX -= velocidade;
+		imgTroncoBaixo.TranslationX -= velocidade;
+		if (imgTroncoBaixo.TranslationX<-larguraJanela)
+		{
+			imgTroncoBaixo.TranslationX = 0 ;
+			imgTroncoCima.TranslationX = 0 ;
+
+		}
+	}
+
 
 
 
